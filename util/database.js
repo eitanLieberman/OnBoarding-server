39a264ed0bac5928754e2ca78b274c22c2e0a410
eitanLieberman <eitanl95@gmail.com>
2022-02-14 09:13:27 +0200

    heroku

--- util/database.js
+++ util/database.js
@@ -4,11 +4,11 @@ const mysql = require("mysql2");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
-  `onboarding`,
-  "guestuser",
+  `heroku_1679b0c0118b04e`,
+  "bd7687ba71b28a",
   `${process.env.MYSQL}`,
   {
-    host: "Eitans-MacBook-Air.local",
+    host: "eu-cdbr-west-02.cleardb.net",
     port: "3306",
     dialect: "mysql",
   }
